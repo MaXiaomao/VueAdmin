47cc39204254527434316574c64e33112dae3d0c
MaXiaomao <maxiaomaoemail@foxmail.com>
2022-02-17 16:31:53 +0800

    chore: 修改cz配置文件

--- .cz-config.js
+++ .cz-config.js
@@ -6,11 +6,12 @@ module.exports = {
 		{value: "style", name: "格式: 代码格式化"},
 		{value: "refactor", name: "重构: 代码重构"},
 		{value: "perf", name: "性能: 优化性能"},
-		{value: "build", name: "构建: 构建流程、外部依赖变更"},
+		{value: "build", name: "构建: 构建流程、外部依赖的更改"},
+		{ value: "chore", name: "配置: 配置文件、辅助工具的更改" },
 	],
 	messages: {
 		type: "选择一种你的提交类型:",
-		subject: "描述:",
+		subject: "简单描述:",
 		confirmCommit: "确定提交?"
 	},
 	allowCustomScopes: true,
